70c722feda9d10d55cb68e7cf86a4e9cde2156e4
LucasSimon <lucas.oscar.simon@gmail.com>
2017-08-22 22:05:00 -0300

    A very big sum - Solved

--- AVeryBigSum/Program.cs
+++ AVeryBigSum/Program.cs
@@ -8,8 +8,19 @@ namespace AVeryBigSum
 {
     class Program
     {
-        static void Main(string[] args)
+        static long aVeryBigSum(int n, long[] ar)
         {
+            // Complete this function
+            return ar.Sum();
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            long[] ar = Array.ConvertAll(ar_temp, Int64.Parse);
+            long result = aVeryBigSum(n, ar);
+            Console.WriteLine(result);
         }
     }
 }
